f18b32001d8bbbbcde9549b504c3727edb220ea3
luiizsilverio <luiiz.silverio@gmail.com>
2023-03-18 20:10:17 -0300

    Desafio concluído

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { FlatList, SafeAreaView, StatusBar, StyleSheet } from "react-native"
+import { FlatList, SafeAreaView, StatusBar, StyleSheet, View } from "react-native"
+import { Picker } from "@react-native-picker/picker";
 
 import NotaEditor from "./src/componentes/NotaEditor";
 import Nota from "./src/componentes/Nota";
@@ -8,6 +9,7 @@ import { buscaNotas, criaTabela } from "./src/services/Notas";
 export default function App() {
   const [notas, setNotas] = useState([]);
   const [notaSel, setNotaSel] = useState({});
+  const [categoria, setCategoria] = useState("*");
 
   useEffect(() => {
     criaTabela();
@@ -18,16 +20,26 @@ export default function App() {
       await mostraNotas();
     }
     inicNotas();
-  }, []);
-
+  }, [categoria]);
 
   async function mostraNotas() {
-    const notes = await buscaNotas();
+    const notes = await buscaNotas(categoria);
     setNotas(notes);
   }
 
   return (
     <SafeAreaView style={estilos.container}>
+      <View style={estilos.modalPicker}>
+        <Picker
+          selectedValue={categoria}
+          onValueChange={(itemValue) => setCategoria(itemValue)}
+        >
+          <Picker.Item label="Todos" value="*" />
+          <Picker.Item label="Pessoal" value="Pessoal" />
+          <Picker.Item label="Trabalho" value="Trabalho" />
+          <Picker.Item label="Outros" value="Outros" />
+        </Picker>
+      </View>
       <FlatList
         data={notas}
         renderItem={(nota) => (
@@ -51,5 +63,11 @@ const estilos = StyleSheet.create({
 		alignItems: "stretch",
 		justifyContent: "flex-start",
 	},
+  modalPicker: {
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: "#EEEEEE",
+    margin: 12
+  },
 })
 
